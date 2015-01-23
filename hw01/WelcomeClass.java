f68a8575bbf497fb21925945767aba002e64239f
kaylynnj13 <kej216@lehigh.edu>
2015-01-23 21:21:55 +0000

    hw01 WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////////
+//Kaylynn Johnson
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//          javac HelloWorld.java
+//  run the program
+//          java HelloWorld//
+//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+    
+    //Print the welcome and lehigh username 
+    System.out.println("  -------------  ");
+    System.out.println("  |  WELCOME   | ");
+    System.out.println("  -------------  ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  ");
+    System.out.println("<-K--E--J--2--1--6->  ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This summer I researched catalysis at the University of Wisconsin.");
+    System.out.println("While I was there, I learned how to sail and windsurf!");
+    
+    
+    }
+}
