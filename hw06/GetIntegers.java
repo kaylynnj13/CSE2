96bbbf686029720d2f06a15606303b29bacb292f
kaylynnj13 <kej216@lehigh.edu>
2015-02-27 01:04:39 +0000

    hw06 Get Integers

---
+++ hw06/GetIntegers.java
@@ -0,0 +1,38 @@
+// Kaylynn Johnson
+// 02/26/2015
+// hw06 Get Integers
+// The purpose of this program is to take in 5 inetegers from the user. and print
+//      the sum of the integers using a for loop
+
+import java.util.Scanner;
+
+public class GetIntegers{
+    //start main method required for every java program
+    public static void main(String[] args){
+        Scanner scan = new Scanner( System.in );
+        //Initialize numbers
+        int num = 0;
+        int sum = 0;
+        // Ask user for 5 valid integers
+        System.out.print("Please enter 5 non-negative integers: ");
+        boolean isInt = scan.hasNextInt();
+        //Create for loop to read 5 integers
+        for( int x = 1; x <= 5; x++ ){
+            //Check if input is an integer
+            while( !isInt ){
+                System.out.println("Invalid input.  Please enter again: ");
+                scan.next();
+                isInt = scan.hasNextInt();
+            }
+            //Check if input is greater or equal to zero
+            num = scan.nextInt();
+            while( num < 0 ){
+                System.out.println("Invalid input.  Please enter again: ");
+                num = scan.nextInt();
+            }
+            sum = sum + num;
+        }
+        //Print the sum of the 5 integers
+        System.out.println("Sum is " + sum);
+    }
+}
